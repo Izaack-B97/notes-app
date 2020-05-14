bc30cc4e27de680302f838b75a6c93b28bfaf023
Isaac Bustamante <izaack.97@gmail.com>
2020-05-14 15:26:58 -0700

    Se agrego la uri de la bd en produccion

--- src/database.js
+++ src/database.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 
 // const NOTES_APP_MONGODB_HOST = process.env.NOTES_APP_MONGODB_HOST;
 // const NOTES_APP_MONGODB_DATABASE = process.env.NOTES_APP_MONGODB_DATABASE;
-
-// const NOTES_APP_MONGODB_HOST = 'localhost'
-// const NOTES_APP_MONGODB_DATABASE = 'nodes-app';
-
-const MONGODB_URI = `mongodb+srv://isaac:1234@notes-app-brqi7.mongodb.net/test?retryWrites=true&w=majority`;
-
-// console.log(MONGODB_URI);
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, 
     { 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 require('./database');
 const app = require('./server');
 
